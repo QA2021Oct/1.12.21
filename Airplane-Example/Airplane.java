dcbeca3cbed4b110f0fb8b7357694bdaa7b6c2a5
QA2021Oct <92875352+QA2021Oct@users.noreply.github.com>
2021-12-06 17:25:16 +0200

    Update Airplane.java

--- Airplane-Example/Airplane.java
+++ Airplane-Example/Airplane.java
@@ -2,12 +2,21 @@ package com.company;
 
 public class Airplane {
 
-    int flight;
+       int flight;
     String destination;
     double speed;
     double distance;
 
     // ALT + INSERT
+    public  Airplane(){
+
+    }
+
+    public Airplane(int flight, String destination) {
+        this.flight = flight;
+        this.destination = destination;
+    }
+
     public Airplane(int flight, String destination, double speed, double distance) {
         this.flight = flight;
         this.destination = destination;
@@ -15,10 +24,19 @@ public class Airplane {
         this.distance = distance;
     }
 
+    @Override
     public String toString(){
         return "flight = " + flight + "\n" +
                 "destination = " + destination +"\n" +
                 "speed = " + speed + "\n" +
                 "distance = " +distance ;
+    }
+
+    public void fly(){
+        System.out.println("fly...");
+    }
+
+    public void fly(int a){
+        System.out.println("fly...");
     }
 }
