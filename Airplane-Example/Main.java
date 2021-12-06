070d08b9c082ad592a3734abdead5517ee407f48
QA2021Oct <92875352+QA2021Oct@users.noreply.github.com>
2021-12-06 17:24:46 +0200

    Update Main.java

--- Airplane-Example/Main.java
+++ Airplane-Example/Main.java
@@ -11,7 +11,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //1:
         Airplane airBus = new Airplane(5325 , "TLV", 420, 3200 );
+        // 2:
+        Airplane airBus2 = new Airplane(888 , "BUD", 420, 3200 );
+        //3:
+        Airplane airBus3 = new Airplane(7891 , "NYC", 420, 3200 );
+        //4:
+        Airplane airBus4 = new Airplane(523 , "PAR", 420, 3200 );
+        //5:
+        Airplane airBus5 = new Airplane(110 , "TLV", 420, 3200 );
+
+        airBus.fly();
+        airBus.fly();
+
         System.out.println(airBus);
 
     }
